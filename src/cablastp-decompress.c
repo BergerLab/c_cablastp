9b4b6d6a617f2d58934cc54ef56552c9a21824e1
Andy Haskell <xcrnr2013@gmail.com>
2014-01-13 12:43:48 -0500

    Decompression now once again works for all of the first sequence

--- src/cablastp-decompress.c
+++ src/cablastp-decompress.c
@@ -72,8 +72,8 @@ main(int argc, char **argv)
         for (link = (compressed[i])->links; link != NULL; link = link->next) {
             /*If link -> diff[1] is a null terminator, this means this link is
               to a chunk added without any matches*/
-            int start = ((link->diff[0] & (char)0x80) == (char)0) ? 100 : 0;
-fprintf(stderr, "%d %d\n", start, link->diff[0]);
+            int start = ((link->diff[0] & (char)0x80) == (char)0) || prev_match ? 100 : 0;
+fprintf(stderr, "%d %d '%s' %d %d\n", start, link->diff[0], link->diff, link->coarse_start, link->coarse_end);
             /*if (!prev_match && start == 100)
                 start--;*/
             struct cbp_seq *chunk =
@@ -88,7 +88,6 @@ fprintf(stderr, "%d %d\n", start, link->diff[0]);
                 printf("%s", read_edit_script(link->diff, chunk->residues+start, length-start));
             prev_match = start == 0;
             current_chunk++;
-if(current_chunk==197)break;
         }
         putc('\n', stdout);
     }
